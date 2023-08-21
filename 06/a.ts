526814f2448018a49d974c41373873f58b2e395f
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-08-21 15:26:07 +0000

    06 complete

---
+++ 06/a.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import readline from 'readline';
+
+class C {
+    constructor() { };
+
+    cScore = 0;
+    buffer = '';
+    resetSize = 4;
+
+    process(line: string) {
+        for (var i=0; i<line.length; i++) {
+            this.buffer += line.charAt(i);
+            console.log('buffer is now: ' + this.buffer);
+            if (this.buffer.length > this.resetSize) this.buffer = this.buffer.substring(1); 
+            console.log('buffer reset : ' + this.buffer);
+            if (this.buffer.length == this.resetSize && this.allDifferent()) { console.log(i+1); this.cScore = i+1; return; }
+        }
+    }
+
+    allDifferent() {
+        return this.buffer.split('').every((c, i, a) => a.indexOf(c) === i);
+    }
+}
+
+var c = new C();
+
+var fn = process.argv[2];
+const rl = readline.createInterface({ input: fs.createReadStream(fn), crlfDelay: Infinity, terminal: false});
+
+rl.on('line', (line) => {
+    c.process(line);
+})
+
+rl.on('close', () => {
+    console.log('Final score: ', c.cScore);
+});
