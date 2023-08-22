5973bc73dc6a6a7f431fd0ea7feb7f20d04be70b
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-08-22 10:49:34 +0000

    07 complete

---
+++ 07/a.ts
@@ -0,0 +1,108 @@
+import fs from 'fs';
+import readline from 'readline';
+
+class DirEntry {
+    constructor(public name: string, public parent?: Directory, private size=0) { };
+    getSize() { return this.size; }
+    getType() { return 'file'; }
+    print(offset='') { console.log(offset + ' - ' + this.name + ' (' + this.getType() + ', size=' + this.getSize() + ')'); }
+}
+
+class Directory extends DirEntry {
+    contents: Array<DirEntry> = new Array();
+    getSize() { return this.contents.reduce((size, e) => size + e.getSize(), 0); }
+    getType() { return 'dir'; }
+    print(offset='') {
+        super.print(offset);
+        this.contents.forEach((e) => {
+            e.print(offset+'  ');
+        });
+    }
+    filter(criteria: (dirEntry: DirEntry) => boolean): Array<DirEntry> {
+        var result = Array<DirEntry>();
+        if (criteria(this)) result.push(this);
+        this.contents.forEach((e) => {
+            if (e instanceof Directory) {
+                result = result.concat(e.filter(criteria));
+            }
+        });
+        return result;
+    }
+}
+
+class C {
+    constructor() {};
+
+    cScore = 0;
+    root = new Directory('/'); // the root directory
+    currentD = this.root;
+
+    processCommand(line: string) {
+        line = line.substring(2);
+        if (line.substring(0, 2) == 'cd') {
+            // change to directory
+            var name = line.split(' ')[1];
+            if (name === '..') {
+                this.currentD = this.currentD.parent ?? this.root
+            } else if (name === '/') {
+                this.currentD = this.root;
+            } else {
+                var newD = this.currentD.contents.find((e) => e.name == name);
+                if (newD && newD instanceof Directory) {
+                    this.currentD = newD;
+                } else {
+                    console.error('Tried to change to non-existent directory:', name);
+                }
+            }
+            //console.log('Current directory is now ' + this.currentD.name);
+        } else {
+            // only other choice is ls
+            // that's the only type of output that will be procesed, so no need to set state
+        }
+    }
+
+    processOutput(line: string) {
+        var [size, name] = line.split(' ');
+        var newE: DirEntry;
+        if (size !== 'dir') {
+            newE = new DirEntry(name, this.currentD, Number(size));
+        } else {
+            newE = new Directory(name, this.currentD);
+        }
+        this.currentD.contents.push(newE);
+    }
+
+    debug() {
+        console.log('-----------------');
+        this.root.print();
+        console.log('-----------------');
+    }
+}
+
+var c = new C();
+
+var fn = process.argv[2];
+const rl = readline.createInterface({ input: fs.createReadStream(fn), crlfDelay: Infinity, terminal: false});
+
+rl.on('line', (line) => {
+    // if line starts with $, it's a command
+    // otherwise it is the output from a directory listing
+    if (line.charAt(0) === '$') c.processCommand(line);
+    else                        c.processOutput(line);
+    //c.debug();
+})
+
+rl.on('close', () => {
+    var matchDirectories = c.root.filter((e) => {
+        return e.getSize() <= 100000;
+    })
+    /*
+    matchDirectories.forEach((e) => {
+        console.log('Directory ' + e.name + ' size is ' + e.getSize());
+    })
+    */
+
+    var matchSize = matchDirectories.reduce((acc, e) => acc + e.getSize(), 0);
+    console.log('sum of matching sizes = ' + matchSize);
+
+});
