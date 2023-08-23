af05edcb45d083db98f2346344948b00c80c0d73
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-08-23 08:50:49 +0000

    10 completed

---
+++ 10/b.ts
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import readline from 'readline';
+
+class C {
+    constructor() { };
+
+    X = 1;
+    cycle = 0;
+
+    process(line: string) {
+        if (line === 'noop') this.tick();
+        else {
+            // addx ###
+            var [cmd, arg] = line.split(' ');
+            var v = Number(arg);
+            this.tick(); 
+            this.tick(); 
+            this.X += v;
+        }
+    }
+
+    tick() {
+        this.cycle++;
+        this.onTick(this.cycle, this.X);
+    }
+
+    onTick(cycle: number, X: number) {
+        console.log(`CPU(${cycle}) X=${X}`);
+    }
+
+    getResult() {
+        return 0;
+    }
+
+    debug() {
+    }
+}
+
+var c = new C();
+var result = 0;
+
+c.onTick = (cycle: number, X: number) => {
+    var column = (cycle-1)%40;
+    if (Math.abs(column-X) <= 1) process.stdout.write('█');
+    else                         process.stdout.write('.');
+
+    if (column == 39) process.stdout.write('\n');
+};
+
+var fn = process.argv[2];
+const rl = readline.createInterface({ input: fs.createReadStream(fn), crlfDelay: Infinity, terminal: false});
+
+rl.on('line', (line) => {
+    c.process(line);
+})
+
+rl.on('close', () => {
+    console.log(`Result: ${result}`);
+});
