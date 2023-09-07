ca1d4bf16e5a3ce6198d3acd7b73d5fcdb29a6a7
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-07 12:02:57 +0000

    09 done, adding tsp for traveling salesman problem

---
+++ 2015/09/b.ts
@@ -0,0 +1,26 @@
+import { Puzzle } from "../../lib/puzzle";
+import { TSP } from "../../lib/tsp";
+
+const p = new Puzzle(process.argv[2]);
+
+let tsp = new TSP();
+
+p.onLine = (line) => {
+    const arr = line.split(' ');
+    console.log(line);
+    tsp.addEdge(arr[0], arr[2], Number(arr[4]));
+};
+
+p.onClose = () => {
+    console.log();
+
+    let shortestPath = tsp.getShortestPath();
+    let longestPath = tsp.getLongestPath();
+
+    console.log(`Shortest path length is ${tsp.getPathCost(shortestPath)}`);
+    console.log(`Longest path length is ${tsp.getPathCost(longestPath)}`);
+    // 443 is too low
+    // 874 is too low
+};
+
+p.run();
