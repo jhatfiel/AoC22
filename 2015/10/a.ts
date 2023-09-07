0d154b5f1d748e940112bdf7290caec80b61b868
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-07 13:07:41 +0000

    10 done

---
+++ 2015/10/a.ts
@@ -0,0 +1,35 @@
+import { Puzzle } from "../../lib/puzzle";
+
+const p = new Puzzle(process.argv[2]);
+
+p.onLine = (line) => {
+    let iteration = 0;
+    while (iteration < 51) {
+        console.log(`[${iteration.toString().padStart(2, ' ')}]: [line] length is ${line.length}`);
+
+        let newStr = '';
+        let lastC = ''
+        let cCnt = 0;
+
+        for (const c of line.split('')) {
+            if (lastC !== '' && c !== lastC) {
+                newStr += cCnt + lastC;
+
+                cCnt = 1;
+            } else {
+                cCnt++;
+            }
+            lastC = c;
+        }
+
+        newStr += cCnt + lastC;
+        line = newStr;
+
+        iteration++;
+    }
+};
+
+p.onClose = () => {
+};
+
+p.run();
