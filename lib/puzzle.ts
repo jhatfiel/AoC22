165c7fc8bda10c659ab021be497d1a32f5d28300
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-15 08:59:58 +0000

    24 done - missed an optimization until part b
    Also adding mask helper functions to puzzle

--- lib/puzzle.ts
+++ lib/puzzle.ts
@@ -13,7 +13,31 @@ export class Puzzle {
     onLine(line: string) { console.log(`Line: ${line}`); }
 
     onClose() { console.log(`Closed`); }
+    
+    toMask(arr: Array<string>, set: Set<string>): number {
+        return arr.map((n, i) => set.has(n)?1<<i:0).reduce((p, v) => p | v, 0);
+    }
 
+    toSet(arr: Array<string>, mask: number): Set<string> {
+        return new Set(arr.filter((v, ind) => 1<<ind & mask));
+    }
+
+    maskOr(arr: Array<string>, mask: number, el: string): number {
+        return mask | (1<<arr.indexOf(el));
+    }
+
+    toMaskN(arr: Array<number>, set: Set<number>): number {
+        return arr.map((n, i) => set.has(n)?1<<i:0).reduce((p, v) => p | v, 0);
+    }
+
+    toSetN(arr: Array<number>, mask: number): Set<number> {
+        return new Set(arr.filter((v, ind) => 1<<ind & mask));
+    }
+    
+    maskOrN(arr: Array<number>, mask: number, el: number): number {
+        return mask | (1<<arr.indexOf(el));
+    }
+    
     gridAround(row: number, col: number, maxRow: number, maxCol: number): Array<{row: number, col: number}> {
         let result = new Array<{row: number, col: number}>();
         if (row > 0 && col > 0)           result.push({row: row-1, col: col-1});
