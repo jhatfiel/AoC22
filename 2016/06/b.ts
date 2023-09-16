ab08b079a023703600449530773707d1d975af48
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-16 19:35:56 +0000

    06 done

---
+++ 2016/06/b.ts
@@ -0,0 +1,25 @@
+import { Puzzle } from "../../lib/puzzle";
+
+const p = new Puzzle(process.argv[2]);
+let positionCharacters = Array.from({length: 10}, () => new Array<string>());
+
+p.onLine = (line) => {
+    console.log(line);
+    line.split('').forEach((c, ind) => {
+        positionCharacters[ind].push(c);
+    });
+};
+
+p.onClose = () => {
+    let msg = '';
+    positionCharacters.forEach((arr) => {
+        let posArr = p.countOccurrences(arr);
+
+        let c = Array.from(posArr.keys()).sort((a, b) => posArr.get(a)! - posArr.get(b)!)[0];
+        if (c) msg += c;
+    });
+    console.log(`The message is ${msg}`);
+
+};
+
+p.run();
