9a7ccd949bf4f0073e81d3b7cc27658bab53108f
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-21 09:03:44 +0000

    14 cleanup and playing around

--- src/2016/14/b.ts
+++ src/2016/14/b.ts
@@ -1,18 +1,18 @@
-//import pkg from 'spark-md5';
-//const { hash } = pkg;
-import { createHash } from 'crypto';
+import pkg from 'spark-md5';
+const { hash } = pkg;
 
 //const SALT='abc'; // 'ahsbgdzn';
 const SALT='ahsbgdzn';
 const cache = new Map<number, string>();
-const _cache = new Map<string, string>();
 const OFFSETS_1000 = Array.from({length: 1000}, (_, i) => i+1);
 const NEED_COUNT = 64;
 
 let found = 0;
 let i = 0;
+let CHAR = ['-', '\\', '|', '/'];
 while (found < NEED_COUNT) {
     if (process.stdout.moveCursor) {
+        process.stdout.clearLine(0);
         console.log(`[${i.toString().padStart(8, ' ')}]`);
         process.stdout.moveCursor(0, -1);
     }
@@ -20,11 +20,14 @@ while (found < NEED_COUNT) {
     // check for 3 characters repeating
     const r = repeating(hex, 3);
     if (r !== undefined) {
+        if (process.stdout.moveCursor) {
+            console.log(`[${i.toString().padStart(8, ' ')}] ${cache.size}`);
+            process.stdout.moveCursor(0, -1);
+        }
         // check for string of 5 of those characters in next 1000 hashes
-        //console.log(`[${i.toString().padStart(8, ' ')}] ${hex} has repeating ${r}`);
         if (OFFSETS_1000.some((o) => getHex(i+o).indexOf(r+r+r+r+r) !== -1)) {
             found++;
-            console.log(`[${i.toString().padStart(8, ' ')}]/[${found.toString().padStart(2, ' ')}] ${hex} has repeating ${r} AND something after it does too!`);
+            console.log(`[${i.toString().padStart(8, ' ')}]/[${found.toString().padStart(2, ' ')}] ${hex}`);
         }
     }
     i++;
@@ -33,11 +36,8 @@ while (found < NEED_COUNT) {
 function getHex(i: number): string {
     let hex = cache.get(i);
     if (hex === undefined) {
-        //let buffer = createHash('md5').update(SALT+i).digest();
-        hex = createHash('md5').update(SALT+i).digest('hex');
-        for (let i=0; i<2016; i++) hex = createHash('md5').update(hex).digest('hex');
-        //for (let i=0; i<2015; i++) buffer = createHash('md5').update(buffer).digest();
-        //hex = createHash('md5').update(buffer).digest('hex');
+        hex = hash(SALT+i);
+        for (let i=0; i<2016; i++) hex = hash(hex);
         cache.set(i, hex);
     }
     return hex;
