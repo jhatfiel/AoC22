6d188d8423fab76e86eb177169e9686fa4dea855
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-29 07:44:20 +0000

    Extracting the positionAfter function to puzzle

--- src/2017/13/bTriangleWave.ts
+++ src/2017/13/bTriangleWave.ts
@@ -4,22 +4,14 @@ const p = new Puzzle(process.argv[2]);
 let depths = new Map<number, number>();
 
 p.onLine = (line) => {
-    console.log(line);
     let [ind, d] = line.split(': ').map(Number);
-    depths.set(ind, d);
+    depths.set(ind, d-1);
 }
 
 p.onClose = () => {
     for (let delay=0; ; delay++) {
-        if (!Array.from(depths.keys()).some((s) => positionAfter(depths.get(s)-1, s+delay) === 0)) { console.log(`Wait ${delay}`); break; };
+        if (!Array.from(depths.keys()).some((s) => p.positionAfter(depths.get(s), s+delay) === 0)) { console.log(`Wait ${delay}`); break; };
     }
-}
-
-function positionAfter(max: number, delay=0): number {
-    return triangleWave(max/2, max*2, delay-max/2)+max/2;
-}
-function triangleWave(amp: number, period: number, delay=0): number {
-    return 4*amp/period * Math.abs((((delay-period/4)%period)+period)%period - period/2) - amp;
 }
 
 p.run();
