78c4620802a0c09fa8cd7faa823e176596d9e71f
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-09-29 10:32:04 +0000

    14 done, extracting some common logic

---
+++ src/2017/14/a.ts
@@ -0,0 +1,20 @@
+import { Cluster } from "../../lib/cluster.js";
+import { Grid } from "../../lib/grid.js";
+import { knotHash } from "../10/knotHash.js";
+
+let grid = new Grid({x: 127, y: 127});
+let input = 'hxtvlmkl'; // real input
+//input='flqrgnkx'; // sample
+
+for (let y=0; y<128; y++) {
+    knotHash(`${input}-${y}`)
+        .split('')
+        .map((d) => Number('0x'+d).toString(2).padStart(4, '0').split(''))
+        .flat()
+        .forEach((b, ind) => grid.grid[y][ind] = (b==='1'));
+}
+
+let nodes = grid.getWalls().map((p) => grid.toKey(p));
+console.log(`Total used: ${nodes.length}`);
+let cluster = new Cluster(nodes, (node) => grid.getNeighbors(node, true));
+console.log(`Number of clusters ${cluster.sets.length}`);
