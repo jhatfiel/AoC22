a4ce3cfa7507f9e23326570dbc2d9b8d804ee84b
jhatfiel <jdhatfield@gmail.com>
2023-12-01 06:45:11 +0100

    01 done

--- src/2023/01/a.ts
+++ src/2023/01/a.ts
@@ -4,8 +4,19 @@ const puzzle = new Puzzle(process.argv[2]);
 
 await puzzle.run()
     .then((lines: Array<string>) => {
+        let sum = 0;
+
         lines.forEach(l => {
-            console.log(l);
+            let first: number = undefined;
+            let last: number = undefined;
+            l.split('').map(Number).forEach(v => {
+                if (Number.isInteger(v)) {
+                    if (first === undefined) first = v;
+                    last = v;
+                }
+            });
+            let value = first*10 + last;
+            sum += value;
         });
-        console.log(lines.map(Number).reduce((acc, n) => acc+n, 0));
+        console.debug(sum);
     });
