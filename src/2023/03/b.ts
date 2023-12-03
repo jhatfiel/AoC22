8d4f5daf47d432ad577a67fd66d5f5b11fbfc69d
jhatfiel <jdhatfield@gmail.com>
2023-12-03 15:06:34 +0100

    03 done

--- src/2023/03/b.ts
+++ src/2023/03/b.ts
@@ -2,6 +2,7 @@ import { Puzzle } from "../../lib/puzzle.js";
 
 const puzzle = new Puzzle(process.argv[2]);
 let grid = new Array<Array<string>>();
+let potGear = new Map<string, Array<number>>();
 
 await puzzle.run()
     .then((lines: Array<string>) => {
@@ -30,9 +31,7 @@ await puzzle.run()
                 } else {
                     // ok not number, really end of a number, do something
                     if (numLeft !== undefined) {
-                        if (checkForSymbol(rowNum, numLeft, numRight)) {
-                            total += Number(row.slice(numLeft, numRight+1).join(''));
-                        }
+                        checkForSymbol(rowNum, numLeft, numRight);
 
                         numLeft = undefined;
                         numRight = undefined;
@@ -40,32 +39,48 @@ await puzzle.run()
                 }
             }
             if (numLeft !== undefined) {
-                if (checkForSymbol(rowNum, numLeft, numRight)) {
-                    total += Number(row.slice(numLeft, numRight+1).join(''));
-                }
+                checkForSymbol(rowNum, numLeft, numRight);
             }
-
         }
+
+        potGear.forEach((numArr, key) => {
+            console.debug(`potGear ${key} has numbers: ${numArr}`)
+            if (numArr.length === 2) {
+                total += numArr[0]*numArr[1];
+            }
+        })
 
         console.log(`Final total is: ${total}`)
     });
 
+function addToPotGear(row: number, col: number, num: number) {
+    let key = `${row},${col}`
+    //console.debug(`PotGear at ${row},${col} found when looking at ${num} (current state of potGear is: ${potGear.get(key)})`);
+    let numArr = potGear.get(key);
+    if (numArr === undefined) {
+        numArr = new Array<number>();
+        potGear.set(key, numArr);
+    }
+    numArr.push(num);
+    //console.debug(`After adding this number, potGear is: ${potGear.get(key)}`);
+}
+
 function checkForSymbol(rowNum: number, left: number, right: number) {
     let foundSymbol = false;
+    let thisNumber = Number(grid[rowNum].slice(left, right+1).join(''));
     if (left !== undefined) {
-        //console.debug(`rowNum=${rowNum}, ${left} to ${right} is ${grid[rowNum].slice(left, right+1).join('')}`)
-        if (left > 0 && grid[rowNum][left-1] !== '.') foundSymbol = true;
-        if (right < grid[rowNum].length-1 && grid[rowNum][right+1] !== '.') foundSymbol = true;
+        if (left > 0 && grid[rowNum][left-1] === '*') addToPotGear(rowNum, left-1, thisNumber)
+        if (right < grid[rowNum].length-1 && grid[rowNum][right+1] === '*') addToPotGear(rowNum, right+1, thisNumber)
         if (rowNum > 0) {
             // check rowNum - 1
             for (let i=left-1; i<=right+1; i++) {
-                if (i > 0 && i < grid[rowNum-1].length-1 && grid[rowNum-1][i] !== '.') foundSymbol = true;
+                if (i > 0 && i < grid[rowNum-1].length-1 && grid[rowNum-1][i] === '*') addToPotGear(rowNum-1, i, thisNumber)
             }
         }
         if (rowNum < grid.length-1) {
             // check rowNum + 1
             for (let i=left-1; i<=right+1; i++) {
-                if (i > 0 && i < grid[rowNum+1].length-1 && grid[rowNum+1][i] !== '.') foundSymbol = true;
+                if (i > 0 && i < grid[rowNum+1].length-1 && grid[rowNum+1][i] === '*') addToPotGear(rowNum+1, i, thisNumber)
             }
         }
     }
