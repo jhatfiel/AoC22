b6c289e28a287f2bc8399836bfb5c8234290a428
jhatfiel <jdhatfield@gmail.com>
2023-12-05 07:23:24 +0100

    05 done - really slow
    there has to be a better way...

--- src/2023/05/a.ts
+++ src/2023/05/a.ts
@@ -1,11 +1,48 @@
 import { Puzzle } from "../../lib/puzzle.js";
 
+type mapRange = {
+    from: number;
+    to: number;
+    length: number;
+}
+
 const puzzle = new Puzzle(process.argv[2]);
+let sArray: Array<number>;
+let maps = Array.from({length: 7}, () => new Array<mapRange>());
 
 await puzzle.run()
     .then((lines: Array<string>) => {
-        lines.forEach(l => {
-            console.log(l);
+        sArray = lines[0].split(':')[1].trim().split(' ').map(Number);
+        console.debug(sArray);
+
+        let mapNum = 0;
+        lines.slice(2).forEach(line => {
+            if (line === '') mapNum++;
+            else if (!line.endsWith('map:')) {
+                // process mapping numbers
+                let arr = line.split(' ').map(Number);
+                maps[mapNum].push({from: arr[1], to: arr[0], length: arr[2]}
+            }
         });
-        console.log(lines.map(Number).reduce((acc, n) => acc+n, 0));
+
+        let lowestLocation = Infinity;
+        sArray.forEach(sNum => {
+            // map through each mapping
+            console.debug(`Working with seed ${sNum}`)
+            maps.forEach((mArr, mNum) => {
+                let newNum = sNum;
+                mArr.forEach(mapping => {
+                    if (sNum >= mapping.from && sNum < mapping.from+mapping.length) {
+                        newNum = mapping.to + sNum-mapping.from;
+                        console.debug(`[${mNum}] Found mapping: ${JSON.stringify(mapping)} Translating ${sNum} to ${newNum}`)
+                    }
+                })
+                sNum = newNum;
+            })
+            if (sNum < lowestLocation) {
+                lowestLocation = sNum;
+                console.log(`New lowest: ${sNum}`)
+            }
+        })
+        console.log(`Final lowest: ${lowestLocation}`)
     });
