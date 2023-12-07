c589eaf295edbc2cb1dc2ff7b6fa60cb0d6e1f08
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-12-07 13:24:44 +0000

    adding GridParser
    refactored 2023/03 to use GridParser
    started 2018/13 using GridParser

---
+++ src/2023/03/bGridParser.ts
@@ -0,0 +1,21 @@
+import { Puzzle } from "../../lib/puzzle.js";
+import { GridParser } from '../../lib/gridParser.js';
+
+const puzzle = new Puzzle(process.argv[2]);
+
+await puzzle.run()
+    .then((lines: Array<string>) => {
+        let gridParser = new GridParser(lines, [GridParser.NUMBERS, new RegExp(/\*/g)]);
+        let total = 0;
+        let potGearArr = gridParser.matches.filter(m => m.typeIndex === 1);
+
+        potGearArr.forEach(potGear => {
+            let neighbors = gridParser.getNeighbors(potGear, 0).map(gpm => Number(gpm.value));
+            if (neighbors.length === 2) {
+                console.debug(`Found a gear at ${potGear.row}, ${potGear.first} with neighbors ${neighbors}`)
+                total += neighbors[0] * neighbors[1];
+            }
+        })
+
+        console.log(`Final total is: ${total}`)
+    });
