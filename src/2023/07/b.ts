96055306249f642ed61de78cbe3dea788cc09f2e
jhatfiel <jdhatfield@gmail.com>
2023-12-07 06:58:14 +0100

    07 done

--- src/2023/07/b.ts
+++ src/2023/07/b.ts
@@ -3,21 +3,43 @@ import { Puzzle } from "../../lib/puzzle.js";
 const puzzle = new Puzzle(process.argv[2]);
 
 const CARD_VALUE = new Map<string, number>();
-CARD_VALUE.set('2', CARD_VALUE.size+2);
-CARD_VALUE.set('3', CARD_VALUE.size+2);
-CARD_VALUE.set('4', CARD_VALUE.size+2);
-CARD_VALUE.set('5', CARD_VALUE.size+2);
-CARD_VALUE.set('6', CARD_VALUE.size+2);
-CARD_VALUE.set('7', CARD_VALUE.size+2);
-CARD_VALUE.set('8', CARD_VALUE.size+2);
-CARD_VALUE.set('9', CARD_VALUE.size+2);
-CARD_VALUE.set('T', CARD_VALUE.size+2);
-CARD_VALUE.set('J', CARD_VALUE.size+2);
+CARD_VALUE.set('J', CARD_VALUE.size+1);
+CARD_VALUE.set('2', CARD_VALUE.size+1);
+CARD_VALUE.set('3', CARD_VALUE.size+1);
+CARD_VALUE.set('4', CARD_VALUE.size+1);
+CARD_VALUE.set('5', CARD_VALUE.size+1);
+CARD_VALUE.set('6', CARD_VALUE.size+1);
+CARD_VALUE.set('7', CARD_VALUE.size+1);
+CARD_VALUE.set('8', CARD_VALUE.size+1);
+CARD_VALUE.set('9', CARD_VALUE.size+1);
+CARD_VALUE.set('T', CARD_VALUE.size+1);
 CARD_VALUE.set('Q', CARD_VALUE.size+2);
 CARD_VALUE.set('K', CARD_VALUE.size+2);
 CARD_VALUE.set('A', CARD_VALUE.size+2);
 
 function getType(cards: string): number {
+    return getBestType(cards);
+}
+
+function getBestType(cards: string): number {
+    let firstJ = cards.indexOf('J');
+    if (firstJ !== -1) {
+        //console.debug(`finding best type for ${cards}`)
+        let cardArr = cards.split('');
+        let bestType = -Infinity;
+        Array.from(CARD_VALUE.keys()).slice(1).forEach(tryCard => {
+            cardArr[firstJ] = tryCard;
+            let type = getBestType(cardArr.join(''));
+            //console.debug(`   trying ${cardArr.join('')}=${type}`)
+            if (type > bestType) bestType = type;
+        })
+        return bestType;
+    } else {
+        return _getType(cards);
+    }
+}
+
+function _getType(cards: string): number {
     let oCnt = puzzle.countOccurrences(cards.split(''));
     let vArr = Array.from(oCnt.values())
     if (oCnt.size === 1) return 6; // 5 of a kind
@@ -54,7 +76,7 @@ await puzzle.run()
             return 0;
         })
         .forEach((hand, index, arr) => {
-            console.debug(`Rank: ${index+1}, Cards: ${hand.cards}, Bid: ${hand.bid}`)
+            console.debug(`Rank: ${index+1}, Cards: ${hand.cards}, type: ${hand.type}, Bid: ${hand.bid}`)
             winnings += hand.bid * (index+1);
         })
         console.debug(`Total Winnings: ${winnings}`)
