c8ecb75b95c47f42ea8479f38730c9a6dcae9307
jhatfiel <jdhatfield@gmail.com>
2023-12-08 07:56:14 +0100

    08 part 1 done

--- src/2023/08/a.ts
+++ src/2023/08/a.ts
@@ -4,8 +4,18 @@ const puzzle = new Puzzle(process.argv[2]);
 
 await puzzle.run()
     .then((lines: Array<string>) => {
-        lines.forEach(l => {
-            console.log(l);
+        const TURNS = lines[0].split('').map(t => t==='L'?0:1);
+        const NEXT = new Map<string, Array<string>>();
+        lines.slice(2).forEach(line => {
+            let [from, directions] = line.split(' = ');
+            NEXT.set(from, directions.replace(/[()]/g, '').split(', '));
         });
-        console.log(lines.map(Number).reduce((acc, n) => acc+n, 0));
+        
+        let steps = 0;
+        let curNode = 'AAA';
+        while (curNode !== 'ZZZ') {
+            curNode = NEXT.get(curNode)[TURNS[steps%TURNS.length]];
+            steps++;
+            console.debug(`[${steps}]: Moving to ${curNode}`)
+        };
     });
