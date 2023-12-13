8bb0d89e878598fb9dda7c184db30e2131727e50
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-12-13 13:06:12 +0000

    12 desc

--- src/2023/12/b.ts
+++ src/2023/12/b.ts
@@ -37,10 +37,10 @@ function possibleArrangements(criteria: string, damagedArray: Array<number>): nu
         return criteria.split('').every(c => c === '?' || c === '.')?1:0;
     } else {
         let result = 0;
-        let p1Pattern = ''.padStart(damagedArray[0], '#'); // part 1 pattern: a string of damaged characters (###...) with length equal to first damagedArray entry
+        let p1Pattern = ''.padStart(damagedArray[0], '#'); // part 1 pattern: a string of damaged characters (###[...]#) with length equal to first damagedArray entry
         let remainingDamagedArray = damagedArray.slice(1); // part 2 will need to match the rest of the damaged array
         if (remainingDamagedArray.length > 0) p1Pattern += '.'; // if we have more damagedArray entries, the part1 string HAS to end with a period
-        // we need to make sure there are at least enough characters left in part2 to fulfil the optimistic matching of the remainingDamagedArray
+        // we need to make sure there are at least enough characters left in part2 to fulfill the optimistic matching of the remainingDamagedArray
         let p2TotalNeeded = remainingDamagedArray.reduce((acc, d) => acc += d, 0) + remainingDamagedArray.length-1;
         // find the index of the first damaged character. Part1 has to contain this character.
         let firstDamagedIndex = criteria.indexOf('#');
