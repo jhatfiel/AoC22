475e52f08755e061ec97384d0737b048303ff972
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-12-15 14:31:38 +0000

    13 done, had fun with visualization on this one

--- src/2018/13/b.ts
+++ src/2018/13/b.ts
@@ -2,7 +2,7 @@ import { Puzzle } from '../../lib/puzzle.js';
 import { SimulationController } from './simulationController.js';
 
 const puzzle = new Puzzle(process.argv[2]);
-const simController = new SimulationController();
+const simController = new SimulationController(false);
 await puzzle.run()
     .then((lines: Array<string>) => {
         // Initialize the simulation
