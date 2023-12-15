81429a45b30ab8ef60c65f128e199957624442e9
jhatfiel <jdhatfield@gmail.com>
2023-12-15 06:44:18 +0100

    15 done

--- src/2023/15/a.ts
+++ src/2023/15/a.ts
@@ -4,8 +4,23 @@ const puzzle = new Puzzle(process.argv[2]);
 
 await puzzle.run()
     .then((lines: Array<string>) => {
-        lines.forEach(l => {
-            console.log(l);
+        lines.forEach(line => {
+            let totalHash = 0;
+            line.split(',').forEach(piece => {
+                let hash = getHash(piece);
+                totalHash += hash;
+            })
+            console.log(`Total Hash: ${totalHash}`)
         });
-        console.log(lines.map(Number).reduce((acc, n) => acc+n, 0));
     });
+
+function getHash(str: string) {
+    let result = 0;
+    str.split('').forEach(c => {
+        let asciiCode = c.charCodeAt(0);
+        result += asciiCode;
+        result *= 17
+        result = result % 256;
+    });
+    return result;
+}
