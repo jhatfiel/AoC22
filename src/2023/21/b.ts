1e679940819b461276cc17a8d287d81ae53766ea
jhatfiel <jdhatfield@gmail.com>
2023-12-21 08:38:17 +0100

    21 part 1 done, part 2 started

--- src/2023/21/b.ts
+++ src/2023/21/b.ts
@@ -4,13 +4,14 @@ import { Puzzle } from "../../lib/puzzle.js";
 const puzzle = new Puzzle(process.argv[2]);
 
 let stepNum = 0;
-let stepsNeeded = 64;
+let stepsNeeded = 300;
 await puzzle.run()
     .then((lines: Array<string>) => {
         let gp = new GridParser(lines, [/#/g, /S/g]);
         let possible = new Set<string>();
-        possible.add(PairToKey(gp.matches.filter(m => m.typeIndex === 1)[0]));
+        //possible.add(PairToKey(gp.matches.filter(m => m.typeIndex === 1)[0]));
         let rocks = new Set<string>(gp.matches.filter(m => m.typeIndex === 0).map(gpm => PairToKey({x: gpm.x, y: gpm.y})));
+        possible.add('0,0');
 
         for (stepNum = 0; stepNum < stepsNeeded; stepNum++) {
             debugPossible(possible);
@@ -30,6 +31,10 @@ await puzzle.run()
 
 function debugPossible(possible: Set<string>) {
     console.debug(`After: ${stepNum} steps, total possible = ${possible.size}`);
+    /*
+    possible.forEach((v) => {
+        if (v.startsWith('0,')) console.debug(`edge ${v}`)
+    });
     /*
     let str = '';
 
