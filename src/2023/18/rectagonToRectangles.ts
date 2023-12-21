059238531c1b4aae587c35a42b0d77717cd60c48
Jamie Hatfield <jamie.hatfield@parsons.com>
2023-12-21 14:09:33 +0000

    18 working through the many edge cases (ha ha)

--- src/2023/18/rectagonToRectangles.ts
+++ src/2023/18/rectagonToRectangles.ts
@@ -5,8 +5,20 @@ export type Rectangle = {TL: Pair, BR: Pair};
 export function RectagonToRectangles(rectagon: Array<Pair>): Array<Rectangle> {
     let result = new Array<Rectangle>();
 
+    let xmin =  Infinity; let ymin =  Infinity;
+    let xmax = -Infinity; let ymax = -Infinity;
+
+    rectagon.forEach(p => {
+        xmin = Math.min(xmin, p.x); ymin = Math.min(ymin, -p.y);
+        xmax = Math.max(xmax, p.x); ymax = Math.max(ymax, -p.y);
+    })
+
+    let additionalOptionsStr = `"xmin":${xmin},"ymin":${ymin},"xmax":${xmax},"ymax":${ymax},`;
+
+    generateGraph(rectagon, `Count (initial)`, additionalOptionsStr);
+
     let count = 0;
-    while (rectagon.length && count < 10) {
+    while (rectagon.length) {// && count < 10) {
         count++;
         console.debug(`Count: ${count}`);
         let minY = rectagon.reduce((my, p) => my = Math.min(my, p.y), Infinity);
@@ -44,7 +56,7 @@ export function RectagonToRectangles(rectagon: Array<Pair>): Array<Rectangle> {
 
         console.debug(`minX: ${minX}, minY: ${minY}, Rx=${Rx}, By=${By}`);
 
-        let nextPoint = rectagon.filter(p => !GridParser.PairEqual(TL, p) && !GridParser.PairEqual(TR, p) && !GridParser.PairEqual(BL, p) && p.x > TL.x && p.x <= TR.x)
+        let nextPoint = rectagon.filter(p => !GridParser.PairEqual(TL, p) && !GridParser.PairEqual(TR, p) && p.x >= TL.x && p.x <= Rx && p.y <= By)
                                 .sort((a, b) => (a.y !== b.y)?a.y-b.y:a.x-b.x)[0];
 
         let rect: Rectangle = {TL: {...TL}, BR: undefined}; // copy TL off for this rectangle, it might change
@@ -57,7 +69,7 @@ export function RectagonToRectangles(rectagon: Array<Pair>): Array<Rectangle> {
             // cleanup the points - remove TL, TR, BL, nextPoint
             rectagon = rectagon.filter(p => !GridParser.PairEqual(TL, p) && !GridParser.PairEqual(TR, p) && !GridParser.PairEqual(BL, p) && !GridParser.PairEqual(nextPoint, p));
         } else if (nextPoint.x === TR.x) {
-            // case 2
+            // case 2 correct on c1, c5, c6
             console.debug(`Case 2, nextPoint: ${PairToKey(nextPoint)}`);
             rect.BR = {x: Rx, y: nextPoint.y-1};
             result.push(rect);
@@ -65,37 +77,46 @@ export function RectagonToRectangles(rectagon: Array<Pair>): Array<Rectangle> {
             rectagon = rectagon.filter(p => !GridParser.PairEqual(TR, p) && !GridParser.PairEqual(nextPoint, p));
 
             TL.y = nextPoint.y;
-        } else if (nextPoint.y === BL.y && nextPoint.x > BL.x) {
-            // case 3
+        } else if (nextPoint.y === BL.y && nextPoint.x >= BL.x) {
             if (rectagon.filter(p => p.y === BL.y && p.x < BL.x).length % 2 === 0) {
+                // case 3a correct on c4
                 console.debug(`Case 3a, nextPoint: ${PairToKey(nextPoint)} (even number of points to the left, BL has to be a bottom left of a box)`);
-                rect.BR = {x: nextPoint.x-1, y: By};
+                rect.BR = {x: nextPoint.x, y: By-1};
                 result.push(rect);
     
-                rectagon = rectagon.filter(p => !GridParser.PairEqual(BL, p) && !GridParser.PairEqual(nextPoint, p));
-    
-                TL.x = nextPoint.x;
-            } else {
-                console.debug(`Case 3b, nextPoint: ${PairToKey(nextPoint)} (odd number of points to the left, entire box can be consumed)`);
+                rectagon = rectagon.filter(p => !GridParser.PairEqual(BL, p) && !GridParser.PairEqual(TL, p));
 
-                rect.BR = {x: Rx, y: By};
-                result.push(rect);
-                let BR = rectagon.filter(p => GridParser.PairEqual(rect.BR, p))[0];
+                if (rectagon.some(p => GridParser.PairEqual({x: Rx, y: By}, p))) {
+                    rectagon = rectagon.filter(p => !GridParser.PairEqual(BL, p) && !GridParser.PairEqual({x: Rx, y: By}, p));
+                    rect.BR.y++;
+                }
 
-                console.debug(`Filtering from ${rectagon.length}`);
-                rectagon = rectagon.filter(p => !GridParser.PairEqual(TL, p) && !GridParser.PairEqual(TR, p));
-                console.debug(`.... to ${rectagon.length}`);
-                BL.x--;
+                TR.y = nextPoint.y;
+                rectagon.filter(p => p.y === nextPoint.y && p.x > TL.x && p.x <= TR.x).forEach(p => p.y++);
                 nextPoint.y++;
-                BR.x = BL.x;
+            } else {
+                // case 3b correct on c2
+                console.debug(`Case 3b, nextPoint: ${PairToKey(nextPoint)} (odd number of points to the left, entire box can be consumed)`);
+                rect.BR = {x: Rx, y: By-1};
+                result.push(rect);
+
+                rectagon = rectagon.filter(p => !GridParser.PairEqual(TL, p) && !GridParser.PairEqual(TR, p));
+                if (rectagon.some(p => GridParser.PairEqual({x: Rx, y: By}, p))) {
+                    rectagon = rectagon.filter(p => !GridParser.PairEqual(BL, p) && !GridParser.PairEqual({x: Rx, y: By}, p));
+                    rect.BR.y++;
+                }
+
+                let BR = BL;
+                BR.x = TR.x;
                 BR.y = nextPoint.y;
+
             }
         } else if (nextPoint.x < TR.x && nextPoint.y < BL.y) {
             // 4 - consume + over to R and down to 4.  Update +y to 4y+1, Ry to 4y+1, and any other points between +x and Rx that are at 4y should move to 4y+1
+            // seems correct on c4
             console.debug(`Case 4, nextPoint: ${PairToKey(nextPoint)}`);
             rect.BR = {x: Rx, y: nextPoint.y};
             result.push(rect);
-
 
             if (rectagon.filter(p => p.y === nextPoint.y && p.x === TR.x).length) {
                 rectagon = rectagon.filter(p => !GridParser.PairEqual(TR, p) && !GridParser.PairEqual({x: TR.x, y: nextPoint.y}, p));
@@ -113,19 +134,29 @@ export function RectagonToRectangles(rectagon: Array<Pair>): Array<Rectangle> {
 
             rectagon = rectagon.filter(p => !GridParser.PairEqual(TL, p) && !GridParser.PairEqual(TR, p));
             BL.x = Rx;
+            //rectagon.filter(p => p.y === BL.y && p.x > TL.x && p.x < Rx).forEach(p => p.y++);
+            //BL.y++;
         }
 
-        generateGraph(rectagon);
+        generateGraph(rectagon, `Count ${count}`, additionalOptionsStr);
     }
     return result;
 }
 
-export function generateGraph(rectagon: Array<Pair>) {
+export function generateGraph(rectagon: Array<Pair>, title: string = '', additionalOptionsStr: string = '') {
     /**
      * Generate input for https://www.graphreader.com/plotter
      */
     let link = 'https://www.graphreader.com/plotter';
-    console.debug(`Generating input for ${link}`);
+    console.debug(`Generating plot code for ${link}`);
+    let str = `{"title": "${title}",` +
+            `"linewidth": 2,` +
+            `"linecolor": "#d18b2c",` +
+            `"markerradius": 2,` +
+            `"markertype": "cross",` +
+            `"chart_size_x": 1400,` +
+            `"chart_size_y": 800,` +
+            additionalOptionsStr;
     if (rectagon.length) {
         let xStr = '';
         let yStr = '';
@@ -133,7 +164,8 @@ export function generateGraph(rectagon: Array<Pair>) {
             xStr += `${p.x},`;
             yStr += `${-p.y},`;
         })
-        console.debug(`{"x":[${xStr}${rectagon[0].x}],"y":[${yStr}${-rectagon[0].y}]}`);
+        str += `"data_x":[${xStr}${rectagon[0].x}],"data_y":[${yStr}${-rectagon[0].y}]}`;
+        console.debug(`\n${str}\n`);
     } else {
         console.debug(`No points to show!`)
     }
