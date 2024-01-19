c5094bbcde5e02aa5be580383e5a54dbfb864943
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-01-19 15:07:05 +0000

    Starting on 2018/15

--- src/2018/15/a.ts
+++
@@ -1,8 +0,0 @@
-import { Puzzle } from '../../lib/puzzle.js';
-
-const puzzle = new Puzzle(process.argv[2]);
-await puzzle.run()
-    .then((lines: Array<string>) => {
-        lines.forEach((line) => {
-        })
-    });
---
+++ src/2018/15/a201815.ts
@@ -0,0 +1,13 @@
+import { AoCPuzzle } from "../../lib/AoCPuzzle";
+
+export class a201815 extends AoCPuzzle {
+    sampleMode(): void { }
+
+    _loadData(lines: string[]) {
+    }
+
+    _runStep(): boolean {
+        this.result = "Result";
+        return false;
+    }
+}
