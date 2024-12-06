04ee264fc5c67429bb3c96299739da74b0cfe786
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-12-06 14:48:01 +0000

    06 done but still slow

--- src/2024/06/b202406.ts
+++ src/2024/06/b202406.ts
@@ -26,38 +26,47 @@ export class b202406 extends AoCPuzzle {
 
     _loadData(lines: string[]) {
         this.gp = new GridParser(lines, [/#/g, /[\^>v<]/g]);
-        this.log(this.gp.matches.map(match => `${match.typeIndex} @${match.x},${match.y}, ${match.value}`).join('\n'));
+        //this.log(this.gp.matches.map(match => `${match.typeIndex} @${match.x},${match.y}, ${match.value}`).join('\n'));
         let startMatch = this.gp.matches.filter(match=>match.typeIndex===1)[0];
         this.walls = this.gp.matches.filter(match=>match.typeIndex===0);
         this.pos = {x: startMatch.x, y: startMatch.y};
         this.dir = DIR.indexOf(startMatch.value);
-        this.log({pos: this.pos}, this.dir);
     }
 
     _runStep(): boolean {
-        //this.log(`at ${this.pos.x},${this.pos.y}`);
+        //this.log(`at ${this.pos.x},${this.pos.y} state=${[...this.state].slice(0,10).join('/')}`);
         this.visited.add(PairToKey(this.pos));
-        this.state.add(PairToKey(this.pos) + ',' + this.dir);
 
         let nextPos = move(this.pos, this.dir);
+        let dir = turnRight(this.dir);
         if (this.walls.some(wall => wall.x === nextPos.x && wall.y === nextPos.y)) {
-            this.dir = turnRight(this.dir);
+            this.dir = dir;
+            this.state.add(PairToKey(this.pos) + ',' + this.dir);
         } else {
-            let rightdir = turnRight(this.dir);
-            let tryPos = {...nextPos};
-            let foundCycle = false;
-            let tryState = new Set<string>();
-            while (!foundCycle && !this.walls.some(wall => wall.x === tryPos.x && wall.y === tryPos.y) &&
-                   tryPos.x >= 0 && tryPos.y >= 0 && tryPos.x < this.gp.width && tryPos.y < this.gp.height) {
-                let trykey = PairToKey(tryPos) + ',' + rightdir;
-                foundCycle = (this.state.has(trykey) || tryState.has(trykey));
-                tryState.add(trykey);
-                tryPos = move(tryPos, rightdir);
-            }
+            // if we've already visited the nextPos, we can't put a rock there, else we wouldn't have gotten here!
+            let nextPosKey = PairToKey(nextPos);
+            if (!this.visited.has(nextPosKey) && !this.loopSpots.has(nextPosKey) && nextPos.x >= 0 && nextPos.y >= 0 && nextPos.x < this.gp.width && nextPos.y < this.gp.height) {
+                let pos = {...this.pos};
+                let foundCycle = false;
+                let tryState = new Set<string>();
+                while (!foundCycle && pos.x >= 0 && pos.y >= 0 && pos.x < this.gp.width && pos.y < this.gp.height) {
+                    //this.log(`  -- TRY at ${tryPos.x},${tryPos.y}`);
+                    let tryNextPos = move(pos, dir);
+                    // if there's a real wall or our trial wall there, turn
+                    if (this.walls.some(wall => wall.x === tryNextPos.x && wall.y === tryNextPos.y) || (tryNextPos.x === nextPos.x && tryNextPos.y === nextPos.y)) {
+                        tryState.add(PairToKey(pos) + ',' + dir);
+                        dir = turnRight(dir);
+                    } else {
+                        pos = tryNextPos;
+                        let tryKey = PairToKey(pos) + ',' + dir;
+                        foundCycle = (this.state.has(tryKey) || tryState.has(tryKey));
+                    }
+                }
 
-            if (foundCycle) {
-                this.log(`Found loop spot! ${nextPos.x},${nextPos.y}`);
-                this.loopSpots.add(PairToKey(nextPos));
+                if (foundCycle) {
+                    this.log(`Found loop spot! ${nextPos.x},${nextPos.y} (loops at ${pos.x},${pos.y})`);
+                    this.loopSpots.add(PairToKey(nextPos));
+                }
             }
 
             this.pos = nextPos;
@@ -66,7 +75,6 @@ export class b202406 extends AoCPuzzle {
         let moreToDo = this.pos.x >=0 && this.pos.y >=0 && this.pos.x < this.gp.width && this.pos.y < this.gp.height;
         if (!moreToDo) {
             this.result = this.loopSpots.size.toString();
-            // 540 is too low
         }
         return moreToDo;
     }
