e4b39a5a0e2c6bb72daecf23b48fdb9530eb788b
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-12-23 14:36:53 +0000

    23 less memory usage

--- src/2024/23/b202423.ts
+++ src/2024/23/b202423.ts
@@ -4,7 +4,7 @@ export class b202423 extends AoCPuzzle {
     con = new Map<string, Set<string>>();
     com: string[] = [];
     count = 0;
-    cliques: {maxIndex: number, nodes: string[], nodeSetArr: Set<string>[]}[][] = [];
+    cliques: {maxIndex: number, nodes: string[], nodeSetArr: Set<string>[]}[] = [];
 
     sampleMode(): void { };
 
@@ -20,38 +20,33 @@ export class b202423 extends AoCPuzzle {
         }
 
         this.com = [...this.con.keys()].sort();
-        this.cliques[1] = [];
+        this.cliques = [];
         for (let i=0; i<this.com.length; i++) {
             let name = this.com[i];
-            this.cliques[1].push({maxIndex: i, nodes: [name], nodeSetArr: [this.con.get(name)]});
-        }
-    }
-
-    expandCliques(size: number) {
-        let nextSize = [];
-        this.cliques[size+1] = nextSize;
-        for (let clique of this.cliques[size]) {
-            for (let i=clique.maxIndex+1; i<this.com.length; i++) {
-                let nextName = this.com[i];
-                if (clique.nodeSetArr.every(nodeSet=>nodeSet.has(nextName))) {
-                    //this.log(`Found new! ${size+1}`);
-                    nextSize.push({maxIndex: i, nodes: [...clique.nodes, nextName], nodeSetArr: [...clique.nodeSetArr, this.con.get(nextName)]});
-                }
-            }
+            this.cliques.push({maxIndex: i, nodes: [name], nodeSetArr: [this.con.get(name)]});
         }
     }
 
     _runStep(): boolean {
-        this.expandCliques(this.stepNumber);
-        let newSize = this.stepNumber+1;
+        let newCliques: {maxIndex: number, nodes: string[], nodeSetArr: Set<string>[]}[] = [];
+        for (let clique of this.cliques) {
+            for (let i=clique.maxIndex+1; i<this.com.length; i++) {
+                let nextName = this.com[i];
+                if (clique.nodeSetArr.every(nodeSet=>nodeSet.has(nextName))) {
+                    //this.log(`Found new! ${size+1}`);
+                    newCliques.push({maxIndex: i, nodes: [...clique.nodes, nextName], nodeSetArr: [...clique.nodeSetArr, this.con.get(nextName)]});
+                }
+            }
+        }
 
-        let moreToDo = this.cliques[newSize].length > 0;
+        let moreToDo = newCliques.length > 0;
 
-        this.log(`${this.stepNumber}: ${this.cliques[newSize].length}`);
+        this.log(`${this.stepNumber}: ${newCliques.length}`);
 
         if (!moreToDo) {
-            this.result = this.cliques[this.stepNumber][0].nodes.join(',');
+            this.result = this.cliques[0].nodes.join(',');
         }
+        this.cliques = newCliques;
         return moreToDo;
     }
 }
