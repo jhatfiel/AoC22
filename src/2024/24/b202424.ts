89ce4407efa9e3bfb72c4b9faadc0f87c8ae3c87
Jamie Hatfield <jamie.hatfield@parsons.com>
2024-12-24 13:24:14 +0000

    24 trying to solve in general

--- src/2024/24/b202424.ts
+++ src/2024/24/b202424.ts
@@ -1,7 +1,8 @@
 import { AoCPuzzle } from '../../lib/AoCPuzzle.js';
 
 export class b202424 extends AoCPuzzle {
-    toCompute = new Map<string, [string, string, string]>();
+    mapping = new Map<string, [string, string, string]>();
+    usedIn = new Map<string, string[]>();
     value = 0n;
     maxz = 0;
 
@@ -16,7 +17,14 @@ export class b202424 extends AoCPuzzle {
                     break;
                 case 1:
                     let arr = line.split(' ');
-                    this.toCompute.set(arr[4], [arr[0], arr[1], arr[2]]);
+                    this.mapping.set(arr[4], [arr[0], arr[1], arr[2]]);
+                    let usedArr: string[];
+                    usedArr = this.usedIn.get(arr[0]);
+                    if (!usedArr) { usedArr = []; this.usedIn.set(arr[0], usedArr)};
+                    usedArr.push(arr[4]);
+                    usedArr = this.usedIn.get(arr[2]);
+                    if (!usedArr) { usedArr = []; this.usedIn.set(arr[2], usedArr)};
+                    usedArr.push(arr[4]);
                     if (arr[4][0] === 'z') this.maxz = Math.max(this.maxz, Number(arr[4].slice(1)));
                     break;
             };
@@ -27,7 +35,7 @@ export class b202424 extends AoCPuzzle {
     format(node: string): string {
         let result = this.formatCache.get(node);
         if (!result) {
-            let arr = this.toCompute.get(node);
+            let arr = this.mapping.get(node);
             if (arr) {
                 let nodes = [arr[0], arr[2]].sort();
                 let p1 = this.format(nodes[0]);
@@ -57,19 +65,108 @@ export class b202424 extends AoCPuzzle {
         let swapList = []; //['thm', 'z08', 'wss', 'wrm', 'hwq', 'z22'];//, 'grd', 'bfq'];// , 'gbs', 'rpq'];
         let swap = (out1,out2) => {
             swapList.push(out1,out2);
-            let swap = this.toCompute.get(out1);
-            this.toCompute.set(out1, this.toCompute.get(out2));
-            this.toCompute.set(out2, swap);
+            let swap = this.mapping.get(out1);
+            this.mapping.set(out1, this.mapping.get(out2));
+            this.mapping.set(out2, swap);
         }
 
+        /*
         swap('thm', 'z08');
         swap('wss', 'wrm');
-        swap('hwq', 'z22');
         swap('z29', 'gbs');
+        */
+        swap('hwq', 'z22');
 
-        for (let i=0; i<=this.maxz; i++) {
-            let wire = `z${i.toString().padStart(2,'0')}`;
-            this.log(wire, this.format(wire));
+        let changed = true;
+        while (changed) {
+            this.formatCache = new Map<string, string>();
+            changed = false;
+            for (let i=0; i<=this.maxz; i++) {
+                let padi = i.toString().padStart(2, '0');
+                let wire = `z${padi}`;
+                let xwire = `x${padi}`;
+                let mapStr = this.format(wire);
+                this.log(wire, mapStr);
+
+                let arr = mapStr.split(' ');
+
+                let wireMapping = this.mapping.get(wire);
+                if (wireMapping[1] !== 'XOR') {
+                    this.log(`Case 1: Find somebody else with the same inputs as ${wire}, but XOR'ed`);
+                    this.log(wireMapping);
+                    let swapWire = this.usedIn.get(wireMapping[0]).filter(w => w!==wire)[0];
+                    this.log(`Swapping ${wire} and ${swapWire}`);
+                    swap(wire, swapWire);
+                    changed = true;
+                    break;
+                }
+
+                let inputMapping = this.mapping.get(wireMapping[0]);
+                if (inputMapping === undefined || (inputMapping[0] !== xwire && inputMapping[2] !== xwire)) inputMapping = this.mapping.get(wireMapping[2]);
+                this.log(inputMapping);
+
+                if (inputMapping && inputMapping[1] !== 'XOR') {
+                    this.log(`Case 2: the x/y input is not x^y`);
+                    let o1 = wireMapping[0];
+                    let mapping = this.mapping.get(o1);
+                    if (mapping.indexOf(xwire) === -1) {
+                        o1 = this.mapping.get(wire)[2];
+                        mapping = this.mapping.get(o1);
+                    }
+                    let swapWire = '';
+                    for (let u of this.usedIn.get(xwire)) {
+                        if (this.mapping.get(u)[1] === 'XOR') {
+                            swapWire = u;
+                            break;
+                        }
+                    }
+                    this.log(`Swapping ${o1} and ${swapWire}`);
+                    swap(o1, swapWire);
+                    changed = true;
+                    break;
+
+                }
+
+                continue;
+
+                if (arr.length > 3 && arr[3] !== 'XOR') {
+                    this.log(`Case 1: Find somebody else with the same inputs as ${wire}`);
+                    let o1 = this.mapping.get(wire)[0];
+                    let swapWire = this.usedIn.get(o1).filter(w => w!==wire)[0];
+                    this.log(`Swapping ${wire} and ${swapWire}`);
+                    swap(wire, swapWire);
+                    changed = true;
+                    break;
+                }
+                if (arr[1] !== 'XOR') {
+                    this.log(`Case 2: one of the parents has x&y instead of x^y`);
+                    let wireMapping = this.mapping.get(wire);
+                    this.log(wireMapping);
+                    if (wireMapping[0] === xwire || wireMapping[2] === xwire) {
+                        let swapWire = this.usedIn.get(xwire).filter(w => w!==wire)[0];
+                        this.log(`Swapping ${wire} and ${swapWire}`);
+
+                    } else {
+                        let o1 = wireMapping[0];
+                        let mapping = this.mapping.get(o1);
+                        if (mapping.indexOf(xwire) === -1) {
+                            o1 = this.mapping.get(wire)[2];
+                            mapping = this.mapping.get(o1);
+                        }
+                        let swapWire = '';
+                        for (let u of this.usedIn.get(xwire)) {
+                            if (this.mapping.get(u)[1] === 'XOR') {
+                                swapWire = u;
+                                break;
+                            }
+                        }
+                        this.log(`Swapping ${o1} and ${swapWire}`);
+                        swap(o1, swapWire);
+                        changed = true;
+                    }
+                    break;
+                }
+            }
         }
 
         if (!moreToDo) {
