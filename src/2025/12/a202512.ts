b673754ed9c88f3c8aac61cf5c0cb7e502843b19
Jamie Hatfield <jdhatfield@gmail.com>
2025-12-12 12:50:27 +0100

    12 done

--- src/2025/12/a202512.ts
+++ src/2025/12/a202512.ts
@@ -1,8 +1,8 @@
 import { AoCPuzzle } from '../../lib/AoCPuzzle.js';
 
 export class a202512 extends AoCPuzzle {
-    shapes: {name: string, width: number, height: number, orientations: string[][][]}[] = [
-        {name: 'A', width: 3, height: 3, orientations: [
+    shapes: {name: string, size: number, orientations: string[][][]}[] = [
+        {name: 'A', size: 7, orientations: [
             [`###`.split(''),
              `#..`.split(''),
              `###`.split('')],
@@ -16,7 +16,7 @@ export class a202512 extends AoCPuzzle {
              `#.#`.split(''),
              `###`.split('')],
         ]},
-        {name: 'B', width: 3, height: 3, orientations: [
+        {name: 'B', size: 7, orientations: [
             [`###`.split(''),
              `.#.`.split(''),
              `###`.split('')],
@@ -24,7 +24,7 @@ export class a202512 extends AoCPuzzle {
              `###`.split(''),
              `#.#`.split('')],
         ]},
-        {name: 'C', width: 3, height: 3, orientations: [
+        {name: 'C', size: 6, orientations: [
             [`###`.split(''),
              `##.`.split(''),
              `#..`.split('')],
@@ -38,7 +38,7 @@ export class a202512 extends AoCPuzzle {
              `##.`.split(''),
              `###`.split('')],
         ]},
-        {name: 'D', width: 3, height: 3, orientations: [
+        {name: 'D', size: 5, orientations: [
             [`.##`.split(''),
              `##.`.split(''),
              `#..`.split('')],
@@ -52,7 +52,7 @@ export class a202512 extends AoCPuzzle {
              `##.`.split(''),
              `.##`.split('')],
         ]},
-        {name: 'E', width: 3, height: 3, orientations: [
+        {name: 'E', size: 7, orientations: [
             [`###`.split(''),
              `##.`.split(''),
              `.##`.split('')],
@@ -78,7 +78,7 @@ export class a202512 extends AoCPuzzle {
              `###`.split(''),
              `.##`.split('')],
         ]},
-        {name: 'F', width: 3, height: 3, orientations: [
+        {name: 'F', size: 7, orientations: [
             [`###`.split(''),
              `##.`.split(''),
              `##.`.split('')],
@@ -108,7 +108,7 @@ export class a202512 extends AoCPuzzle {
 
     sampleMode(): void {
         this.shapes = [
-            {name: 'A', width: 3, height: 3, orientations: [
+            {name: 'A', size: 7, orientations: [
                 [`###`.split(''),
                  `##.`.split(''),
                  `##.`.split('')],
@@ -134,7 +134,7 @@ export class a202512 extends AoCPuzzle {
                  `###`.split(''),
                  `###`.split('')],
             ]},
-            {name: 'B', width: 3, height: 3, orientations: [
+            {name: 'B', size: 7, orientations: [
                 [`###`.split(''),
                  `##.`.split(''),
                  `.##`.split('')],
@@ -160,7 +160,7 @@ export class a202512 extends AoCPuzzle {
                  `###`.split(''),
                  `.##`.split('')],
             ]},
-            {name: 'C', width: 3, height: 3, orientations: [
+            {name: 'C', size: 7, orientations: [
                 [`.##`.split(''),
                  `###`.split(''),
                  `##.`.split('')],
@@ -168,7 +168,7 @@ export class a202512 extends AoCPuzzle {
                  `###`.split(''),
                  `.##`.split('')],
             ]},
-            {name: 'D', width: 3, height: 3, orientations: [
+            {name: 'D', size: 7, orientations: [
                 [`##.`.split(''),
                  `###`.split(''),
                  `##.`.split('')],
@@ -182,7 +182,7 @@ export class a202512 extends AoCPuzzle {
                  `###`.split(''),
                  `###`.split('')],
             ]},
-            {name: 'E', width: 3, height: 3, orientations: [
+            {name: 'E', size: 7, orientations: [
                 [`###`.split(''),
                  `#..`.split(''),
                  `###`.split('')],
@@ -196,7 +196,7 @@ export class a202512 extends AoCPuzzle {
                  `#.#`.split(''),
                  `###`.split('')],
             ]},
-            {name: 'F', width: 3, height: 3, orientations: [
+            {name: 'F', size: 7, orientations: [
                 [`###`.split(''),
                  `.#.`.split(''),
                  `###`.split('')],
@@ -209,10 +209,13 @@ export class a202512 extends AoCPuzzle {
     };
 
     regions: {width: number, height: number, presents: number[]}[] = [];
+    easyCount = 0;
+    hardCount = 0;
+    impossibleCount = 0;
 
     _loadData(lines: string[]) {
         lines.filter(l=>l.indexOf('x') != -1).forEach(l => {
-            const match = l.match(/^(\d)+x(\d+): (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)/).map(Number);
+            const match = l.match(/^(\d+)x(\d+): (\d+) (\d+) (\d+) (\d+) (\d+) (\d+)/).map(Number);
             this.regions.push({width: match[1], height: match[2], presents: match.slice(3, 9)})
         })
     }
@@ -220,9 +223,19 @@ export class a202512 extends AoCPuzzle {
     _runStep(): boolean {
         let moreToDo = this.stepNumber < this.regions.length;
         const region = this.regions[this.stepNumber-1];
-        console.log(`Processing region ${this.stepNumber}: ${region.width}x${region.height} with presents ${region.presents.join(', ')}`);    
+        const regionSize = region.height*region.width;
+        const squaresNeeded = region.presents.reduce((sum, n, i) => sum+n*this.shapes[i].size, 0);
+        const blocksNeeded = region.presents.reduce((sum, n) => sum+n, 0);
+        if (regionSize < squaresNeeded) {
+            this.impossibleCount++;
+        } else if (regionSize >= 9*blocksNeeded) {
+            this.easyCount++;
+        } else {
+            console.log(`HARD region ${this.stepNumber}: ${region.width}x${region.height} with presents ${region.presents.join(', ')} Size: ${regionSize} squaresNeeded: ${squaresNeeded}, blocksNeeded: ${blocksNeeded}`);
+            this.hardCount++;
+        }
         if (!moreToDo) {
-            this.result = 'Result';
+            this.result = `Easy: ${this.easyCount} Hard: ${this.hardCount} Impossible: ${this.impossibleCount}`;
         }
         // 250 too low
         // 375 incorrect (no more hints, LOL)
